fb0fd400993bda37668a736481b0f9067ed93cbc
Win Feigle <win.feigle@gmail.com>
2022-10-29 18:20:06 -0600

    Complete module

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,19 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+puts "Creating freebies..."
+5.times {
+    Freebie.create(item_name: "Hoodie", value: 59, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+}
+
+5.times {
+    Freebie.create(item_name: "T-Shirt", value: 19, dev_id: Dev.all.sample.id, company_id: Company.all.sample.id)
+}
+
+Freebie.create(item_name: "$10k Cash Prize", value: 10000, dev_id: Dev.all.sample.id, company_id: Company.find_by(name: "Enron").id)
+
+Freebie.create(item_name: "Dundie Award", value: 10, dev_id: Dev.find_by(name: "Gazorpazop").id, company_id: Company.find_by(name: "Dunder Mifflin").id)
+
+Freebie.create(item_name: "Facebook Verified Badge", value: 1000, dev_id: Dev.find_by(name: "Rick").id, company_id: Company.find_by(name: "Facebook").id)
 
 puts "Seeding done!"
