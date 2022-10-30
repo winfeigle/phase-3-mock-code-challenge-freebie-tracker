778b1a4d456c6b1ec4dc9bb572f3f0d2c7f89374
Win Feigle <win.feigle@gmail.com>
2022-10-29 18:19:31 -0600

    commit tree 94b09f7189204108b3457511d18b629f70272668

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        self.freebies.find_by(item_name: item_name) ? true : false
+
+        # OR YOU COULD DO THIS...
+        # self.freebies.any? do |freebie|
+        #     freebie.item_name == item_name
+        #   end
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev: dev) unless freebie.dev != self
+    end
 end
